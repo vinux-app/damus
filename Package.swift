5f277a43c1450a8b6d404b5dcadce6f2dd3ba6cc
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-07-28 01:30:08 -0400

    Package.swift: add Reachability.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,5 @@
 dependencies: [
     .Package(url: "https://github.com/daltoniam/Starscream.git", majorVersion: 4),
-    .Package(url: "https://github.com/jb55/secp256k1.swift.git", branch: "main")
+    .Package(url: "https://github.com/jb55/secp256k1.swift.git", branch: "main"),
+    .Package(url: "https://github.com/ashleymills/Reachability.swift.git", branch: "v5.1.0")
 ]
