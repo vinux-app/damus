4c9c92260505ebdeca5d36a268cf49cdee81b11d
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-08-08 17:09:16 -0400

    Appinfo.swift config follow up

--- damus/AppInfo.swift
+++ damus/AppInfo.swift
@@ -1,12 +1,12 @@
 // DO NOT EDIT, // IT IS A MACHINE GENERATED FILE
 
-// AppInfo.swift un Aug 7 19:31:02 2022 -0700
+// AppInfo.swift on Aug 8 17:04:20 2022 -0400
 
 import Foundation
 class AppInfo {
 let version: String
 let build: String
-let gitCommitSHA: String = "df5d655"
+let gitCommitSHA: String = "d43b89a"
 init?() {
     guard let version =
 Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String, let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String else { return nil }
